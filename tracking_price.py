becaa73ad5485cadf9182dbd5bdf4c21111e9cae
Masood Qaim <masoodaqaim@gmail.com>
2019-09-26 15:01:03 -0400

    added function

--- tracking_price.py
+++ tracking_price.py
@@ -8,8 +8,21 @@ url = 'https://www.amazon.com/Introducing-Echo-Show-Compact-Charcoal/dp/B07HZLHP
 headers = {'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'}
 page = requests.get(url, headers=headers)  # the call to the webpage
 soup = BeautifulSoup(page.content, 'html.parser')  # saves all webpage data
-
 #print(soup.prettify())  # just to see what it prints
-title = soup.find(id='productTitle')  # need to know the html code/variable. inspect element
-price = soup.find(id='priceblock_ourprice')
-print(price)  # returns a string
+
+
+def check_price():
+    price = soup.find(id='priceblock_ourprice').get_text()
+    # print(price) >> $89.99
+    # print(type(price))  >> class 'str'
+
+    # convert the string into a float and remove the $. cannot compare str to str
+    float_price = float(price[1:])
+    print(float_price)  # >> 89.99
+    print(type(float_price))  # >> class 'float'
+
+    #if float_price < 50:
+        #send_email()
+
+
+check_price()
